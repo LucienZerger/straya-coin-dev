633caeded00bd7ba16ce5fe76cf386fb2ed7ecf5
Dieter T <lucien.zerger@gmail.com>
2018-01-25 16:22:36 +1100

    Removed old hash gen code.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -126,39 +126,6 @@ public:
         //assert(consensus.hashGenesisBlock == uint256S("0x"));
         //assert(genesis.hashMerkleRoot == uint256S("0x"));
 
-// If genesis block hash does not match, then generate new genesis hash.
-       if (false && genesis.GetHash() != hashGenesisBlock)
-       {
-           printf("Searching for genesis block...\n");
-           // This will figure out a valid hash and Nonce if you're
-           // creating a different genesis block:
-           uint256 hashTarget = CBigNum().SetCompact(genesis.nBits).getuint256();
-           uint256 thash;
-           char scratchpad[SCRYPT_SCRATCHPAD_SIZE];
-
-           loop
-           {
-               scrypt_1024_1_1_256_sp_generic(BEGIN(genesis.nVersion), BEGIN(thash), scratchpad);
-               if (thash <= hashTarget)
-                   break;
-               if ((genesis.nNonce & 0xFFF) == 0)
-               {
-                   printf("nonce %08X: hash = %s (target = %s)\n", genesis.nNonce, thash.ToString().c_str(), hashTarget.ToString().c_str());
-               }
-               ++genesis.nNonce;
-               if (genesis.nNonce == 0)
-               {
-                   printf("NONCE WRAPPED, incrementing time\n");
-                   ++genesis.nTime;
-               }
-           }
-           printf("block.nTime = %u \n", genesis.nTime);
-           printf("block.nNonce = %u \n", genesis.nNonce);
-           printf("block.GetHash = %s\n", genesis.GetHash().ToString().c_str());
-       }
-
-
-        genesis.print();
 
 
 
